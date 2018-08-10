59c87e8038b5e1c471cd359535acce36f9b17590
Travis Fischer <fisch0920@gmail.com>
2018-08-09 23:17:58 -0400

    make demo work

--- example/src/App.js
+++ example/src/App.js
@@ -1,12 +1,60 @@
 import React, { Component } from 'react'
+import Calendar from 'react-calendar-pane'
+import moment from 'moment'
+import momentFr from 'moment/locale/fr' // eslint-disable-line
 
-import ExampleComponent from 'react-calendar-pane'
+const customDayRenderer = ({ handleClick, date }) => { // eslint-disable-line
+  return (
+    <a
+      className='Day-inner'
+      href={'#' + date.format('YYYY-MM-DD')}
+      onClick={() => handleClick(date)}
+    >
+      {date.format('D')}
+    </a>
+  )
+}
 
-export default class App extends Component {
-  render () {
+export default class Example extends Component {
+  onSelect(date, previousDate, currentMonth) {
+    if (moment(date).isSame(previousDate)) {
+      console.info('onSelect: false', date)
+      return false
+    } else if (currentMonth.isSame(date, 'month')) {
+      console.info('onSelect: true', date)
+      return true
+    } else {
+      console.info('onSelect: none', date)
+    }
+  }
+
+  render() {
+    let dayClasses = function(date) {
+      let day = date.isoWeekday()
+      if (day === 6 || day === 7) {
+        return ['weekend']
+      }
+      return []
+    }
     return (
       <div>
-        <ExampleComponent text='Modern React component module' />
+        <p>Calendar with weekend</p>
+        <Calendar onSelect={this.onSelect} dayClasses={dayClasses} />
+        <p>Calendar without nav</p>
+        <Calendar
+          onSelect={this.onSelect}
+          dayClasses={dayClasses}
+          useNav={false}
+        />
+        <p>French calendar</p>
+        <Calendar
+          onSelect={this.onSelect}
+          dayClasses={dayClasses}
+          locale='fr'
+          startOfWeekIndex={1}
+        />
+        <p>Calendar with custom day renderer</p>
+        <Calendar onSelect={this.onSelect} dayRenderer={customDayRenderer} />
       </div>
     )
   }
